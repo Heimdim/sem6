56db9b09183a95ef507ec4edb96bc4ece23356d7
Heimdim <dmitriyromanovets@mail.ru>
2018-03-06 18:08:04 +0300

    add rmi server

--- lab3_RMI/Server/src/server/Server.java
+++ lab3_RMI/Server/src/server/Server.java
@@ -1,6 +1,38 @@
 package server;
 
+import services.RemoteServiceImpl;
+
+import java.rmi.AlreadyBoundException;
+import java.rmi.Remote;
+import java.rmi.RemoteException;
+import java.rmi.registry.LocateRegistry;
+import java.rmi.registry.Registry;
+import java.rmi.server.UnicastRemoteObject;
+
 public class Server
 {
+    public static final String BINDING_NAME = "sample/MyService";
 
+    public static void main(String[] args) throws RemoteException, AlreadyBoundException
+    {
+        System.out.print("Starting registry...");
+        Registry registry= LocateRegistry.createRegistry(4996);
+
+        RemoteServiceImpl remoteService=new RemoteServiceImpl();
+        Remote stub= UnicastRemoteObject.exportObject(remoteService,0);
+
+        System.out.print("Binding service...");
+        registry.bind(BINDING_NAME, stub);
+        System.out.println(" OK");
+
+        while (true)
+        {
+            try
+            {
+                Thread.sleep(Integer.MAX_VALUE);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
 }
