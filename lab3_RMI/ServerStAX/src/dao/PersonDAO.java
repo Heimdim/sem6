b35350f8569155d965e15ee639218ae81738ebce
Heimdim <dmitriyromanovets@mail.ru>
2018-04-08 23:35:10 +0300

    add (ServerStAX/dao) PersonDAO interface

---
+++ lab3_RMI/ServerStAX/src/dao/PersonDAO.java
@@ -0,0 +1,20 @@
+package dao;
+
+import entity.Person;
+
+import java.util.List;
+
+public interface PersonDAO
+{
+    boolean addPerson(Person person);
+
+    boolean removePerson(Person person);
+
+    boolean updatePerson(Person person1, Person person2);
+
+    Person getPersonById(int id);
+
+    List<Person> getPersons();
+
+    List<Person> getSuitablePersons(Person person);
+}
