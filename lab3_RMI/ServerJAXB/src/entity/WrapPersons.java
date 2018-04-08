60c7f69723a7fcf0dfd9dfabd290646b11125691
Heimdim <dmitriyromanovets@mail.ru>
2018-04-09 00:51:37 +0300

    add (ServerJAXB/entity/WrapPersons) javadoc comments

--- lab3_RMI/ServerJAXB/src/entity/WrapPersons.java
+++ lab3_RMI/ServerJAXB/src/entity/WrapPersons.java
@@ -7,19 +7,40 @@ import javax.xml.bind.annotation.XmlRootElement;
 import java.io.Serializable;
 import java.util.List;
 
+/**
+ Класс-обертка для списка людей
+
+ @author Dmitriy Romanovets
+ @version 1.0
+ */
+
+
 @XmlRootElement(name = "persons")
 @XmlAccessorType(XmlAccessType.NONE)
 public class WrapPersons implements Serializable
 {
-        @XmlElement(name = "person")
-        private List<Person> persons;
+    /** Поле, хранящее полный список людей */
+    @XmlElement(name = "person")
+    private List<Person> persons;
 
-        public List<Person> getPersons() {
+    /**
+     * Метод для получения полного списка людей
+     *
+     * @return возвращает полный список людей
+     */
+    public List<Person> getPersons()
+    {
             return persons;
-        }
+    }
 
-        public void setPersons(List<Person> persons) {
-            this.persons = persons;
-        }
+    /**
+     * Метод для задания полного списка людей
+     *
+     * @param persons - задаваемый полный список людей
+     */
+    public void setPersons(List<Person> persons)
+    {
+        this.persons = persons;
+    }
 }
 
