331796b03b9727618677522e7e15d57fa26745ac
Heimdim <dmitriyromanovets@mail.ru>
2018-04-08 23:29:35 +0300

    add (ServerStAX/utils) PersonTags enum for XML

--- lab3_RMI/ServerStAX/src/utils/PersonTags.java
+++ lab3_RMI/ServerStAX/src/utils/PersonTags.java
@@ -2,8 +2,8 @@ package utils;
 
 public enum PersonTags
 {
-    PERSONS("persons"), PERSON("person"),ID("id"), AGE("age"), HEIGHT("height"), PREFGENDER("prefGender"),GENDER("gender"),LPREFAGE("lowPrefAge"),
-    hPREFAGE("highPrefAge"), NAME("name");
+    PERSONS("persons"), PERSON("person"),ID("id"), AGE("age"), HEIGHT("height"), PREFGENDER("prefGender"),GENDER("gender"),
+    LPREFAGE("lowPrefAge"), HPREFAGE("highPrefAge"), NAME("name");
 
     private String tag;
 
