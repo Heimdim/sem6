e5bbbd676b623930e0aaa9001824dc51f5a273f2
Heimdim <dmitriyromanovets@mail.ru>
2018-04-15 23:41:30 +0300

    add DefaultServlet.java, default.jsp, default.css

--- lab4_ServletsWizard/src/servlets/DefaultServlet.java
+++ lab4_ServletsWizard/src/servlets/DefaultServlet.java
@@ -2,23 +2,18 @@ package servlets;
 
 
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
 
-public class TestServlet extends HttpServlet
+@WebServlet(name = "StartServlet", urlPatterns = "")
+public class DefaultServlet extends HttpServlet
 {
-    public void processRequest()
-    {
-
-    }
-
     public void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException
     {
-        resp.setContentType("text/html;charset=utf-8");
-        PrintWriter pw = resp.getWriter();
-        pw.println("<H1>Hello, world! или Привет мир</H1>");
+        req.getRequestDispatcher("/jsp_source/default.jsp").forward(req,resp);
     }
 }
---
+++ lab4_ServletsWizard/web/jsp_source/default.jsp
@@ -0,0 +1,15 @@
+<%@ page contentType="text/html; charset=UTF-8" language="java" %>
+
+<html>
+<head>
+    <title>Test</title>
+    <link rel="stylesheet" href="css_source/default.css" type="text/css" />
+</head>
+<body >
+    <form name="startBtn" action="/firstStep" method="get">
+        <div class="container1">
+            <input  class="btn1" type="submit" value="Начать Внеочередной Крестовый Поход"/>
+        </div>
+    </form>
+</body>
+</html>
