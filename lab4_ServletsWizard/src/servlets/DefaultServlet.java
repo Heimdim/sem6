f60f2f2980fc32d4648c65882a7aa33e37dc5d3e
Heimdim <dmitriyromanovets@mail.ru>
2018-04-17 15:55:44 +0300

    correct default step

--- lab4_ServletsWizard/src/servlets/DefaultServlet.java
+++ lab4_ServletsWizard/src/servlets/DefaultServlet.java
@@ -1,19 +1,24 @@
 package servlets;
 
 
+import entities.Crusade;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import java.io.IOException;
-import java.io.PrintWriter;
 
 @WebServlet(name = "StartServlet", urlPatterns = "")
 public class DefaultServlet extends HttpServlet
 {
     public void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException
     {
-        req.getRequestDispatcher("/jsp_source/default.jsp").forward(req,resp);
+        HttpSession session = req.getSession();
+
+        session.setAttribute("crusade", new Crusade());
+        req.getRequestDispatcher("jsp_source/default.jsp").forward(req,resp);
     }
 }
--- lab4_ServletsWizard/web/jsp_source/default.jsp
+++ lab4_ServletsWizard/web/jsp_source/default.jsp
@@ -3,13 +3,11 @@
 <html>
 <head>
     <title>Test</title>
-    <link rel="stylesheet" href="css_source/default.css" type="text/css" />
+    <link rel="stylesheet" href="${pageContext.request.contextPath}/css_source/default.css" type="text/css" />
 </head>
 <body >
-    <form name="startBtn" action="/firstStep" method="get">
-        <div class="container1">
+    <form name="startBtn" action="${pageContext.request.contextPath}/firstStep" method="get">
             <input  class="btn1" type="submit" value="Начать Внеочередной Крестовый Поход"/>
-        </div>
     </form>
 </body>
 </html>
